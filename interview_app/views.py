fbb32fff7db48db8215d040ec0507f18b22eb5dd
LewkowskiArkadiuszTmp <LewkowskiArkadiuszTmp@gmail.com>
2016-07-29 14:48:22 +0200

    dodanie zależności, beautifulsoup4

--- interview_app/views.py
+++ interview_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
-
+from bs4 import BeautifulSoup
+import urllib2
 
 # todo
 def index(request, url_parameter):
@@ -10,6 +11,8 @@ def index(request, url_parameter):
     except:
         raise Http404("Not Found")
     data = {}
+
+
     data['html'] = ''
     data['title'] = ''
     data['description'] = ''
