2128a6695852ceb898b0a5d469039cb0abf2680c
LewkowskiArkadiuszTmp <LewkowskiArkadiuszTmp@gmail.com>
2016-07-29 13:57:21 +0200

    mapowanie urls dla widoku

--- interview_project/urls.py
+++ interview_project/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+#from interview_app import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'interview_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^interview_app/', include('interview_app.urls')),
+                       )
