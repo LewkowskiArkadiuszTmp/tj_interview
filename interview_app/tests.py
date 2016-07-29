4ac701123d0e5278375b617a1cdc50a597e77b1a
LewkowskiArkadiuszTmp <LewkowskiArkadiuszTmp@gmail.com>
2016-07-29 14:21:41 +0200

    test, format json

--- interview_app/tests.py
+++ interview_app/tests.py
@@ -1,10 +1,16 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+
 class IndexViewTests(TestCase):
 
-    # test czy zwraca json
     def index_view_return_empty_json(self):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertJSONEqual(response.content, {})
+
+    def index_view_return_valid_json_format(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {
+                             "html": "", "title": "", "description": ""})
