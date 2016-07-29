78c13002fee17d6d2626f959fa82af31f1d1098d
LewkowskiArkadiuszTmp <LewkowskiArkadiuszTmp@gmail.com>
2016-07-29 14:14:56 +0200

    test, czy widok zwraca json

--- interview_app/tests.py
+++ interview_app/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+class IndexViewTests(TestCase):
+
+    # test czy zwraca json
+    def index_view_return_empty_json(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {})
